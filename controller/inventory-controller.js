9628bc0d7878e118520b9e558e9fdac697c014ce
Ganesh M <ganesh.masilamani@innovatiqconsulting.com>
2024-08-20 09:27:14 +0530

    assetDelete

--- controller/inventory-controller.js
+++ controller/inventory-controller.js
@@ -107,95 +107,195 @@ export const fetchProductCves = async (request, response) => {
   }
 };
 
+// export const deleteAssets = async (request, response) => {
+//   try {
+
+//     for (const asset of request.body.assets) {
+
+//       await Inventory.findByIdAndDelete(asset._id);
+
+//     }
+
+//     return response.status(201).json({
+//       success: true,
+//       message: " Asset(s) deleted successfully",
+//     });
+//   } catch (error) {
+//     console.log(error);
+
+//   }
+// };
+
 export const deleteAssets = async (request, response) => {
   try {
-
     for (const asset of request.body.assets) {
-
-      await Inventory.findByIdAndDelete(asset._id);
-
+      const assetFilter = { vendor: asset.vendor, osType: asset.osType, project:asset.project , partNo: asset.partNo};
+      const assetCount = await Inventory.countDocuments(assetFilter);
+      if (assetCount > 1) {
+        await Inventory.findByIdAndDelete(asset._id);
+      } else {
+        const vendorFilter = { vendorName: asset.vendor, osType: asset.osType, project: asset.project, partNo: asset.partNo };
+        const vendorProducts = await vendorProductCveModel.find(vendorFilter); 
+        for (const product of vendorProducts) {
+          await vendorProductCveModel.findByIdAndDelete(product._id);
+        }
+        await Inventory.findByIdAndDelete(asset._id);
+      }
     }
 
     return response.status(201).json({
       success: true,
-      message: " Asset(s) deleted successfully",
+      message: "Asset(s) deleted successfully",
     });
   } catch (error) {
-    console.log(error);
-
+    console.error(error);
+    return response.status(500).json({
+      success: false,
+      message: "An error occurred while deleting the assets.",
+    });
   }
 };
-
 
 export const uploadAssets = async (req, res, next) => {
   try {
-
-    let path = req.file.path;
-
-    var fileName = req.file.filename;
-    var substr = fileName.split("-")[2];
-    var projectName = substr.split(".")[0];
-    if (req.file == undefined) {
+    if (!req.file) {
       return res.status(400).send("Please upload an excel file!");
     }
 
-    readXlsxFile(path).then(async (rows) => {
+    const path = req.file.path;
+    const fileName = req.file.filename;
+    const substr = fileName.split("-")[2];
+    const projectName = substr.split(".")[0];
 
-      rows.shift();
-      let inventories = [];
-      var totalCount = 0;
-      var insertedCount = 0;
-      for (const row of rows) {
-        totalCount = totalCount + 1;
-        let inventory = {
-          //vendor: row[0].toLowerCase(),
-          vendor: row[0],
-          osType: row[1],
-          partNo: row[2],
-          // product: row[2].toLowerCase(),
-          product: row[3],
-          type: row[4],
-          serialNo: row[5],
-          firmwareVersion: row[6],
-          cpeName: '',
-          vulnarabilities: 0,
-          status: 'A',
-          project: projectName
-        };
-        
-        const inven = await Inventory.findOne({
-          "project":inventory.project,"vendor": inventory.vendor, "partNo": inventory.partNo, "product": inventory.product, "type": inventory.type, "serialNo": inventory.serialNo,
-          "firmwareVersion": inventory.firmwareVersion,"osType":inventory.osType
-        });
+    const rows = await readXlsxFile(path);
 
-        if(inven){
-          console.log("asset already exists")
-            }
-            else {
-          insertedCount = insertedCount + 1;
-          await Inventory.create(inventory);
-        }
+    if (rows.length === 0) {
+      return res.status(400).send("The uploaded file is empty!");
+    }
 
-        if (rows.length == totalCount) {
+    rows.shift(); // Remove the header row
 
-          return res.status(201).json({
-            success: true,
-            message: "Total Excel Records are " + totalCount + " and " + insertedCount + " Asset(s) uploaded successfully",
-          });
-        }
+    let totalCount = 0;
+    let insertedCount = 0;
+    const inventories = [];
 
+    for (const row of rows) {
+      totalCount += 1;
+      const inventory = {
+        vendor: row[0],
+        osType: row[1],
+        partNo: row[2],
+        product: row[3],
+        type: row[4],
+        serialNo: row[5],
+        firmwareVersion: row[6],
+        cpeName: '',
+        vulnarabilities: 0,
+        status: 'A',
+        project: projectName,
+      };
+
+      const existingInventory = await Inventory.findOne({
+        project: inventory.project,
+        vendor: inventory.vendor,
+        partNo: inventory.partNo,
+        product: inventory.product,
+        type: inventory.type,
+        serialNo: inventory.serialNo,
+        firmwareVersion: inventory.firmwareVersion,
+        osType: inventory.osType,
+      });
+
+      if (!existingInventory) {
+        inventories.push(inventory);
+        insertedCount += 1;
       }
+    }
 
+    if (inventories.length > 0) {
+      await Inventory.insertMany(inventories);
+    }
+
+    return res.status(201).json({
+      success: true,
+      message: `Total Excel Records are ${totalCount} and ${insertedCount} Asset(s) uploaded successfully`,
     });
-    // return res.status(201).json({
-    //       success: true,
-    //       message: " Asset(s) uploaded successfully",
-    //     });
-
   } catch (err) {
+    // logger.error(`Error uploading assets: ${err.message}`);
     return res.status(500).json({ success: false, message: err.message });
   }
 };
+
+
+// export const uploadAssets = async (req, res, next) => {
+//   try {
+
+//     let path = req.file.path;
+
+//     var fileName = req.file.filename;
+//     var substr = fileName.split("-")[2];
+//     var projectName = substr.split(".")[0];
+//     if (req.file == undefined) {
+//       return res.status(400).send("Please upload an excel file!");
+//     }
+
+//     readXlsxFile(path).then(async (rows) => {
+
+//       rows.shift();
+//       let inventories = [];
+//       var totalCount = 0;
+//       var insertedCount = 0;
+//       for (const row of rows) {
+//         totalCount = totalCount + 1;
+//         let inventory = {
+//           //vendor: row[0].toLowerCase(),
+//           vendor: row[0],
+//           osType: row[1],
+//           partNo: row[2],
+//           // product: row[2].toLowerCase(),
+//           product: row[3],
+//           type: row[4],
+//           serialNo: row[5],
+//           firmwareVersion: row[6],
+//           cpeName: '',
+//           vulnarabilities: 0,
+//           status: 'A',
+//           project: projectName
+//         };
+        
+//         const inven = await Inventory.findOne({
+//           "project":inventory.project,"vendor": inventory.vendor, "partNo": inventory.partNo, "product": inventory.product, "type": inventory.type, "serialNo": inventory.serialNo,
+//           "firmwareVersion": inventory.firmwareVersion,"osType":inventory.osType
+//         });
+
+//         if(inven){
+//           console.log("asset already exists")
+//             }
+//             else {
+//           insertedCount = insertedCount + 1;
+//           await Inventory.create(inventory);
+//         }
+
+//         if (rows.length == totalCount) {
+
+//           return res.status(201).json({
+//             success: true,
+//             message: "Total Excel Records are " + totalCount + " and " + insertedCount + " Asset(s) uploaded successfully",
+//           });
+//         }
+
+//       }
+
+//     });
+//     // return res.status(201).json({
+//     //       success: true,
+//     //       message: " Asset(s) uploaded successfully",
+//     //     });
+
+//   } catch (err) {
+//     return res.status(500).json({ success: false, message: err.message });
+//   }
+// };
 
 export const updateAsset = async (request, response) => {
   try {
